64b474d0da4b58fbd89517eaabe9e2594e674ae9
NTDKhoa04 <146697000+NTDKhoa04@users.noreply.github.com>
2024-11-30 12:04:58 +0700

    Added data fetch for user

--- frontend/app/src/main/java/com/worthybitbuilders/squadsense/models/report_models/ReportModel.java
+++ frontend/app/src/main/java/com/worthybitbuilders/squadsense/models/report_models/ReportModel.java
@@ -7,12 +7,14 @@ public class ReportModel {
     public List<CheckBox> checkbox;
     public List<TimelineModel> timeline;
     public List<StatusReportModel> status;
+    public List<UserReportModel> user;
 
-    public ReportModel(String title,List<CheckBox> checkbox, List<TimelineModel> timeline, List<StatusReportModel> status){
+    public ReportModel(String title,List<CheckBox> checkbox, List<TimelineModel> timeline, List<StatusReportModel> status, List<UserReportModel> user){
         this.title = title;
         this.checkbox = checkbox;
         this.timeline = timeline;
         this.status = status;
+        this.user = user;
     }
 
 }
